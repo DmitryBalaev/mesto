867fdac76cf5ea3870dd46f0de985bb31de5dd22
Dmitry Balaev <vitalij-borman@yandex.ru>
2023-01-11 21:32:21 +0300

    add trash btn

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,28 @@
 // Объявляем переменные
 const editBtn = document.querySelector('.profile__edit-btn');
+const newCardAddBtn = document.querySelector('.profile__add-btn');
 const closePopupBtn = document.querySelector('.popup__btn-close');
 const popup = document.querySelector('.popup');
+const editPopup = document.querySelector('.popup_edit-profile');
+const popupNewCard = document.querySelector('.popup_new-card');
 const userName = document.querySelector('.profile__user-name');
 const userProfession = document.querySelector('.profile__profession');
 const userNameInput = document.querySelector('.popup__input_user_name');
 const userProfessionInput = document.querySelector('.popup__input_user_profession');
-const profileForm = document.querySelector('.popup__form');
+const profileForm = document.querySelector('.popup__form_edit-profile');
 
-function popupOpen() {
-  popup.classList.add('popup_opened');
+function editPopupOpen() {
+  editPopup.classList.add('popup_opened');
   userNameInput.value = userName.textContent;
   userProfessionInput.value = userProfession.textContent;
+}
+function addCardPopupOpen() {
+  popupNewCard.classList.add('popup_opened');
 }
 function popupClose() {
   popup.classList.remove('popup_opened');
 }
+
 function profileSave(event) {
   event.preventDefault()
   userName.textContent = userNameInput.value;
@@ -23,5 +30,6 @@ function profileSave(event) {
   popupClose();
 }
 profileForm.addEventListener('submit', profileSave);
-editBtn.addEventListener('click', popupOpen);
+newCardAddBtn.addEventListener('click', addCardPopupOpen);
+editBtn.addEventListener('click', editPopupOpen);
 closePopupBtn.addEventListener('click', popupClose);
