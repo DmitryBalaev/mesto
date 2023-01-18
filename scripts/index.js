39120e56fbb7bd88b2d5a25b36ba525bb26ad47e
Dmitry Balaev <vitalij-borman@yandex.ru>
2023-01-18 23:42:54 +0300

    finish him?

--- scripts/index.js
+++ scripts/index.js
@@ -22,90 +22,76 @@ const cardList = document.querySelector('.cards__list')
 const cardItemTemplate = document.querySelector('#card-item').content
 
 // Рендер 6 карточек
-const cardItemContent = initialCards.map(function (item) {
-  return {
-    title : item.name,
-    link : item.link
-  };
-})
-function renderInitialCards() {
-  cardItemContent.forEach(renderCard)
-}
-function renderCard ({title, link}) {
+function createCard(name, link) {
   const cardItem = cardItemTemplate.querySelector('.cards__item').cloneNode(true)
   cardItem.querySelector('.cards__item-img').src = link
-  cardItem.querySelector('.cards__item-title').textContent = title
+  cardItem.querySelector('.cards__item-img').alt = name
+  cardItem.querySelector('.cards__item-title').textContent = name
   cardItem.querySelector('.cards__item-btn').addEventListener('click', function (evt) {
     evt.target.classList.toggle('cards__item-btn_active')
   })
-  cardItem.querySelector('.card__item-trash-btn').addEventListener('click', function (evt){
+  cardItem.querySelector('.card__item-trash-btn').addEventListener('click', function (evt) {
     evt.target.parentElement.remove()
   })
   cardItem.querySelector('.cards__item-img').addEventListener('click', openPopup(popupFullScreen))
   cardItem.querySelector('.cards__item-img').addEventListener('click', function () {
     popupFullScreenImg.src = link
-    popupFullScreenDescriptions.textContent = title
+    popupFullScreenImg.alt = name
+    popupFullScreenDescriptions.textContent = name
   })
-  cardList.append(cardItem)
+  return cardItem
 }
-renderInitialCards();
+ initialCards.forEach((data) => {
+  const card = createCard(data.name, data.link);
+  cardList.append(card);
+})
+
 // Открытие попапов
-function openPopup (popup) {
+function openPopup(popup) {
   return () => {
     popup.classList.add('popup_opened')
     resetPopupAddCardValue()
-  }}
-btnEditProfile.addEventListener('click', openPopup(popupEditProfile))
-btnEditProfile.addEventListener('click', saveProfileText)
+  }
+}
+btnEditProfile.addEventListener('click', function () {
+  openPopup(popupEditProfile)()
+  saveProfileText()
+})
 btnAddCard.addEventListener('click', openPopup(popupAddCard))
+
 // Закрытие попапов
-function closePopup (popup) {
+function closePopup(popup) {
   return () => popup.classList.remove('popup_opened')
 }
-btnPopupClose.addEventListener('click', closePopup (popupEditProfile))
+btnPopupClose.addEventListener('click', closePopup(popupEditProfile))
 popupAddCardCloseBtn.addEventListener('click', closePopup(popupAddCard))
 popupFullScreenCloseBtn.addEventListener('click', closePopup(popupFullScreen))
 // Сохранение текста из инпутов
-function saveProfileText () {
+
+function saveProfileText() {
   popupEditProfileUserNameInput.value = profileUserName.textContent;
   popupEditProfileUserProfessionInput.value = profileProfession.textContent
 }
+
 // Обнуление value
-function resetPopupAddCardValue (){
+function resetPopupAddCardValue() {
   popupAddCardNameInput.value = ''
   popupAddCardImgInput.value = ''
 }
 // Сохранение value инпутов в профиле
-function saveEditProfileValue (evt) {
+function saveEditProfileValue(evt) {
   evt.preventDefault()
   profileUserName.textContent = popupEditProfileUserNameInput.value
   profileProfession.textContent = popupEditProfileUserProfessionInput.value
   closePopup(popupEditProfile)()
 }
 popupEditProfileForm.addEventListener('submit', saveEditProfileValue)
+
 // Добавление новых карточек
-function addNewCard (evt) {
+function addNewCard(evt) {
   evt.preventDefault()
   closePopup(popupAddCard)()
-  renderNewCard()
-}
-function renderNewCard () {
-  const newCard = cardItemTemplate.querySelector('.cards__item').cloneNode(true)
-  const image = popupAddCardImgInput.value
-  const title = popupAddCardNameInput.value
-  newCard.querySelector('.cards__item-img').src = image
-  newCard.querySelector('.cards__item-title').textContent = title
-  newCard.querySelector('.cards__item-btn').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('cards__item-btn_active')
-  })
-  newCard.querySelector('.card__item-trash-btn').addEventListener('click', function (evt){
-    evt.target.parentElement.remove()
-  })
-  newCard.querySelector('.cards__item-img').addEventListener('click', openPopup(popupFullScreen))
-  newCard.querySelector('.cards__item-img').addEventListener('click', function () {
-    popupFullScreenImg.src = image
-    popupFullScreenDescriptions.textContent = title
-  })
+  const newCard = createCard(popupAddCardNameInput.value, popupAddCardImgInput.value)
   cardList.prepend(newCard)
 }
 popupAddCardForm.addEventListener('submit', addNewCard)
