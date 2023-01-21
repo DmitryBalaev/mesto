eeba49f4b06d38a9200fafa452b0863c3c38e5c4
Dmitry Balaev <vitalij-borman@yandex.ru>
2023-01-21 16:22:03 +0300

    GHpages not deployed

--- scripts/constants.js
+++ scripts/constants.js
@@ -24,3 +24,23 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
+const btnEditProfile = document.querySelector('.profile__edit-btn')
+const buttonClosePopupEditProfile = document.querySelector('.popup__btn-close')
+const btnAddCard = document.querySelector('.profile__add-btn')
+const popupEditProfile = document.querySelector('.popup-edit')
+const popupEditProfileUserNameInput = document.querySelector('.popup__input_user_name')
+const popupEditProfileUserProfessionInput = document.querySelector('.popup__input_user_profession')
+const popupEditProfileForm = document.querySelector('.popup__form_edit-profile')
+const popupAddCard = document.querySelector('.popup-add')
+const popupAddCardCloseBtn = popupAddCard.querySelector('.popup__btn-close')
+const popupAddCardForm = popupAddCard.querySelector('.popup__form_new-card')
+const popupAddCardNameInput = popupAddCard.querySelector('.popup__input_new-card_name')
+const popupAddCardImgInput = popupAddCard.querySelector('.popup__input_new-card_link')
+const popupFullScreen = document.querySelector('.popup-image')
+const popupFullScreenDescriptions = popupFullScreen.querySelector('.full-screen__descriptions')
+const popupFullScreenImg = popupFullScreen.querySelector('.full-screen__image')
+const popupFullScreenCloseBtn = popupFullScreen.querySelector('.popup__btn-close')
+const profileUserName = document.querySelector('.profile__user-name')
+const profileProfession = document.querySelector('.profile__profession')
+const cardList = document.querySelector('.cards__list')
+const cardItemTemplate = document.querySelector('#card-item').content
--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,3 @@
-// Объявляем переменные
-const btnEditProfile = document.querySelector('.profile__edit-btn')
-const buttonClosePopupEditProfile = document.querySelector('.popup__btn-close')
-const btnAddCard = document.querySelector('.profile__add-btn')
-const popupEditProfile = document.querySelector('.popup-edit')
-const popupEditProfileUserNameInput = document.querySelector('.popup__input_user_name')
-const popupEditProfileUserProfessionInput = document.querySelector('.popup__input_user_profession')
-const popupEditProfileForm = document.querySelector('.popup__form_edit-profile')
-const popupAddCard = document.querySelector('.popup-add')
-const popupAddCardCloseBtn = popupAddCard.querySelector('.popup__btn-close')
-const popupAddCardForm = popupAddCard.querySelector('.popup__form_new-card')
-const popupAddCardNameInput = popupAddCard.querySelector('.popup__input_new-card_name')
-const popupAddCardImgInput = popupAddCard.querySelector('.popup__input_new-card_link')
-const popupFullScreen = document.querySelector('.popup-image')
-const popupFullScreenDescriptions = popupFullScreen.querySelector('.full-screen__descriptions')
-const popupFullScreenImg = popupFullScreen.querySelector('.full-screen__image')
-const popupFullScreenCloseBtn = popupFullScreen.querySelector('.popup__btn-close')
-const profileUserName = document.querySelector('.profile__user-name')
-const profileProfession = document.querySelector('.profile__profession')
-const cardList = document.querySelector('.cards__list')
-const cardItemTemplate = document.querySelector('#card-item').content
-
 // Рендер 6 карточек
 function createCard(name, link) {
   const cardItem = cardItemTemplate.querySelector('.cards__item').cloneNode(true)
