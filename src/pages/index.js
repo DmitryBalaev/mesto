0500e3221573404648a3905ef6620e11f84e308c
Dmitry Balaev <vitalij-borman@yandex.ru>
2023-03-06 23:56:28 +0300

    refactor: changed fail structure project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,60 +1,9 @@
-import Card from "../scripts/Card.js";
-import FormValidator from "../scripts/FormValidator.js";
-import '../pages/index.css';
+import Card from "../components/Card.js";
+import FormValidator from "../components/FormValidator.js";
+import './index.css';
+import {initialCards, popupItem, btnEditProfile, closeButtons, btnAddCard, popupEditProfile, popupEditProfileUserNameInput, popupEditProfileUserProfessionInput, popupEditProfileForm, popupAddCard, popupAddCardForm, popupAddCardNameInput, popupAddCardImgInput, popupFullScreen, popupFullScreenDescriptions, popupFullScreenImg, profileUserName, profileProfession, cardList, formValidationObj} from '../utils/constants.js';
 
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-const popupItem = document.querySelectorAll('.popup')
-const btnEditProfile = document.querySelector('.profile__edit-btn')
-const closeButtons = document.querySelectorAll('.popup__btn-close')
-const btnAddCard = document.querySelector('.profile__add-btn')
-const popupEditProfile = document.querySelector('.popup-edit')
-const popupEditProfileUserNameInput = document.querySelector('.popup__input_user_name')
-const popupEditProfileUserProfessionInput = document.querySelector('.popup__input_user_profession')
-const popupEditProfileForm = document.querySelector('.popup__form_edit-profile')
-const popupAddCard = document.querySelector('.popup-add')
-const popupAddCardForm = popupAddCard.querySelector('.popup__form_new-card')
-const popupAddCardNameInput = popupAddCard.querySelector('.popup__input_new-card_name')
-const popupAddCardImgInput = popupAddCard.querySelector('.popup__input_new-card_link')
-const popupFullScreen = document.querySelector('.popup-image')
-const popupFullScreenDescriptions = popupFullScreen.querySelector('.full-screen__descriptions')
-const popupFullScreenImg = popupFullScreen.querySelector('.full-screen__image')
-const profileUserName = document.querySelector('.profile__user-name')
-const profileProfession = document.querySelector('.profile__profession')
-const cardList = document.querySelector('.cards__list')
-const cardItemTemplate = document.querySelector('#card-item').content
-const formValidationObj = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  inputErrorClass: 'popup__input_type_error',
-  buttonSelector: '.popup__btn-submit',
-  buttonDisabledClass: 'popup__btn-submit_disabled',
-}
 
 const editProfileValidator = new FormValidator(formValidationObj, popupEditProfileForm);
 editProfileValidator.enableValidation(formValidationObj);
---
+++ src/utils/constants.js
@@ -0,0 +1,51 @@
+export const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+export const popupItem = document.querySelectorAll('.popup')
+export const btnEditProfile = document.querySelector('.profile__edit-btn')
+export const closeButtons = document.querySelectorAll('.popup__btn-close')
+export const btnAddCard = document.querySelector('.profile__add-btn')
+export const popupEditProfile = document.querySelector('.popup-edit')
+export const popupEditProfileUserNameInput = document.querySelector('.popup__input_user_name')
+export const popupEditProfileUserProfessionInput = document.querySelector('.popup__input_user_profession')
+export const popupEditProfileForm = document.querySelector('.popup__form_edit-profile')
+export const popupAddCard = document.querySelector('.popup-add')
+export const popupAddCardForm = popupAddCard.querySelector('.popup__form_new-card')
+export const popupAddCardNameInput = popupAddCard.querySelector('.popup__input_new-card_name')
+export const popupAddCardImgInput = popupAddCard.querySelector('.popup__input_new-card_link')
+export const popupFullScreen = document.querySelector('.popup-image')
+export const popupFullScreenDescriptions = popupFullScreen.querySelector('.full-screen__descriptions')
+export const popupFullScreenImg = popupFullScreen.querySelector('.full-screen__image')
+export const profileUserName = document.querySelector('.profile__user-name')
+export const profileProfession = document.querySelector('.profile__profession')
+export const cardList = document.querySelector('.cards__list')
+export const formValidationObj = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  inputErrorClass: 'popup__input_type_error',
+  buttonSelector: '.popup__btn-submit',
+  buttonDisabledClass: 'popup__btn-submit_disabled',
+}
