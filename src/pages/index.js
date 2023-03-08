fe5405274df05a3043e37ac0136cc23621c2afce
Dmitry Balaev <vitalij-borman@yandex.ru>
2023-03-08 20:56:52 +0300

    final

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,26 +7,32 @@ import UserInfo from "../components/UserInfo.js";
 import './index.css';
 import {initialCards, btnEditProfile, btnAddCard, popupEditProfileUserNameInput, popupEditProfileUserProfessionInput, popupEditProfileForm, popupAddCardForm, popupFullScreenSelector, popupFullScreenDescriptions, popupFullScreenImg, cardListSelector, formValidationObj, UserProfileSelectorObj} from '../utils/constants.js';
 
+// Работа с данными пользователя
 const userInfo = new UserInfo(UserProfileSelectorObj)
 
+// Попап картинки
 const popupWhitImage = new PopupWithImage(popupFullScreenSelector);
 popupWhitImage.setEventListeners();
 
-
+// Попап формы изменения данных Пользователя
 const popupWithFormEdit = new PopupWithForm('.popup-edit', () => {
   const inputValues = popupWithFormEdit.setInputValues();
   userInfo.setUserInfo(inputValues);
   popupWithFormEdit.close()
 })
+
 popupWithFormEdit.setEventListeners()
 
+
+// Попап формы добавления карточки
 const popupWithFormAddCard = new PopupWithForm('.popup-add', () => {
   cardSection.addItem(createCard(popupWithFormAddCard.setInputValues()))
   popupWithFormAddCard.close()
 })
+
 popupWithFormAddCard.setEventListeners()
 
-// Initial 6 card
+// Добавляем на страницу 6 исходных карточек
 const cardSection = new Section({
   items: initialCards.reverse(),
   renderer: (item) => {
@@ -36,24 +42,25 @@ const cardSection = new Section({
 
 cardSection.renderItems();
 
+// Возвращаем разметку карточки со слушателями
 function createCard({name, link}) {
-  const card = new Card(name, link, '#card-item', fillPopupFullScreen)
+  const card = new Card(name, link, '#card-item', (link, name) => {
+    popupFullScreenImg.src = link
+    popupFullScreenImg.alt = name
+    popupFullScreenDescriptions.textContent = name
+    popupWhitImage.open(link, name)
+  })
   const cardElement = card.generateCard();
   return cardElement
 }
-// Form validation
+
+// Валидация форм
 const editProfileValidator = new FormValidator(formValidationObj, popupEditProfileForm);
 editProfileValidator.enableValidation(formValidationObj);
 const addCardValidator = new FormValidator(formValidationObj, popupAddCardForm);
 addCardValidator.enableValidation(formValidationObj);
 
-function fillPopupFullScreen(link, name) {
-  popupFullScreenImg.src = link
-  popupFullScreenImg.alt = name
-  popupFullScreenDescriptions.textContent = name
-  popupWhitImage.open(link, name)
-}
-
+// Слушателеи кнопкок открытия попапов
 btnEditProfile.addEventListener('click', function () {
   const profileValues = userInfo.getUserInfo();
   popupEditProfileUserNameInput.value = profileValues.name;
